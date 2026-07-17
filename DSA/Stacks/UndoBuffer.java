e8397366ca2780c6d636f5c39e4f066e3d812d09
lakshita3103 <lakshita3103@gmail.com>
2026-07-17 16:35:03 +0530

    [Lakshita] Add : Stack Problems

---
+++ DSA/Stacks/UndoBuffer.java
@@ -0,0 +1,55 @@
+package DSA.Stacks;
+
+class UndoBuffer {
+
+    private String[] data;
+    private int top;
+
+    UndoBuffer(int maxDepth) {
+        data = new String[maxDepth];
+        top = -1;
+    }
+
+    public boolean push(String edit) {
+        if (top == data.length - 1) {
+            return false; // Stack Overflow
+        }
+
+        data[++top] = edit;
+        return true;
+    }
+
+    public String pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Nothing to undo");
+        }
+
+        return data[top--];
+    }
+
+    public String peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+
+        return data[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public static void main(String[] args) {
+
+        UndoBuffer stack = new UndoBuffer(3);
+
+        stack.push("Edit1");
+        stack.push("Edit2");
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack.peek());
+    }
+}
